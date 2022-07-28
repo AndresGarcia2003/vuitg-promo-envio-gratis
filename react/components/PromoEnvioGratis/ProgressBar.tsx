14622403f7bfa55b0127d1ce16f313ba4ef32dfc
AndresGarcia2003 <endresfel111150@gmail.com>
2022-07-28 12:44:20 -0500

    feature/solucion axios.get

--- react/components/PromoEnvioGratis/ProgressBar.tsx
+++ react/components/PromoEnvioGratis/ProgressBar.tsx
@@ -3,30 +3,36 @@ import React from 'react'
 import { useCssHandles } from 'vtex.css-handles'
 import './ProgressBar.css'
 
-interface IProgressBar {
+type Props = {
   percentage: number
 }
 
-export const ProgressBar = ({ percentage }: IProgressBar) => {
+export const ProgressBar = ({ percentage }: Props) => {
+  // eslint-disable-next-line no-console
+  console.log(percentage)
+  const LineProgress = (props: { value: number }) => {
+    return <>{`${Math.round(props.value)}%`}</>
+  }
+
   const [style, setStyle] = React.useState({})
 
   setTimeout(() => {
     const newStyle = {
-      opacity:1,
-      width:`${percentage}%`
+      opacity: 1,
+      width: `${percentage}%`,
     }
 
     setStyle(newStyle)
-  },1000)
+  }, 1000)
 
   const CSS_HANDLES = ['progressb', 'progress-done']
   // eslint-disable-next-line padding-line-between-statements
   const handles = useCssHandles(CSS_HANDLES)
   // eslint-disable-next-line padding-line-between-statements
   return (
-    <div className={handles['progressb']}>
-      <div className={handles['progress-done']}style={style}>
-          {percentage}%
+    <div className={handles.progressb}>
+      <div className={handles['progress-done']} style={style}>
+        <LineProgress value={percentage} />
       </div>
     </div>
   )
--- react/components/PromoEnvioGratis/index.tsx
+++ react/components/PromoEnvioGratis/index.tsx
@@ -1,16 +1,41 @@
-/* eslint-disable import/order */
-/* eslint-disable padding-line-between-statements */
 /* eslint-disable prettier/prettier */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
+import axios from 'axios'
+
 import { ProgressBar } from './ProgressBar'
-import { ProgressMessage } from './ProgressMessage'
 
 export const PromoEnvioGratis = () => {
+  // eslint-disable-next-line prettier/prettier
+  const {
+    orderForm: { totalizers },
+  } = useOrderForm()
+  const [totalOrder, setTotalOrder] = useState(0)
+  const [goal, setGoal] = useState(0)
+  const promoId = '31da3c11-b693-43fc-a41d-b42ac4b27062'
+
+  useEffect(() => {
+    axios
+      .get(`https:/api/rnb/pvt/calculatorconfiguration/${promoId}`)
+      .then((response) => {
+        setGoal(response.data.totalValueFloor)
+        setTotalOrder(totalizers[0]?.value / 100)
+      })
+  }, [totalizers])
+
   return (
     <>
-     <ProgressBar percentage={70} />
-     <ProgressMessage completed={false} remainValue={20000} />
+      <ProgressBar
+        percentage={totalOrder < goal ? (totalOrder / goal) * 100 : 100}
+      />
+      {totalOrder >= goal ? (
+        <div>El envio de tu compra es totalmente gratis</div>
+      ) : (
+        <div>
+          Te hace falta {`$${(goal - totalOrder).toLocaleString('de-DE')}`} para
+          disfrutar del envio gratis
+        </div>
+      )}
     </>
-
   )
 }
