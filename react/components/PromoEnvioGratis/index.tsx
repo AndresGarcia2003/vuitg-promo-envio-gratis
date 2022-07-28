0b1c1aaabdedfc5ed1abb79e86f82fe7da2af1de
AndresGarcia2003 <endresfel111150@gmail.com>
2022-07-28 13:39:13 -0500

    feature/url

--- react/components/PromoEnvioGratis/index.tsx
+++ react/components/PromoEnvioGratis/index.tsx
@@ -10,13 +10,14 @@ export const PromoEnvioGratis = () => {
   const {
     orderForm: { totalizers },
   } = useOrderForm()
+
   const [totalOrder, setTotalOrder] = useState(0)
   const [goal, setGoal] = useState(0)
   const promoId = '31da3c11-b693-43fc-a41d-b42ac4b27062'
 
   useEffect(() => {
     axios
-      .get(`https:/api/rnb/pvt/calculatorconfiguration/${promoId}`)
+      .get(`/api/rnb/pvt/calculatorconfiguration/${promoId}`)
       .then((response) => {
         setGoal(response.data.totalValueFloor)
         setTotalOrder(totalizers[0]?.value / 100)
